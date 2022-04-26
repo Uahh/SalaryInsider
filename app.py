cbcffe7407b7f68608f87ba03b4aa00ba15c1ff8
Uahh <hnyrarara@gmail.com>
2022-04-26 23:59:41 +0800

    add flask

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from model import server_config
 rs = redis.Redis(host=server_config.server_ip, port=server_config.redis_port, password=server_config.redis_pw, decode_responses=True)
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+app.jinja_env.variable_start_string = '{['
+app.jinja_env.variable_end_string = ']}'
 print('Waiting......')
 
 
@@ -24,22 +26,25 @@ def index():
 @app.route('/create_room', methods=["POST"])
 def create_room():
     data = json.loads(request.get_data(as_text=True))
-    id =  str(uuid.uuid1())
-    rs.set(id + '_roomNumber', data['roomNumber'], ex=600)  # 10分钟消除
+    id = str(uuid.uuid1())
+    rs.set(id + '_roomPeople', data['roomPeople'], ex=600)  # 10分钟消除
+    rs.set(id + '_nowPeople', "1", ex=600)  # 10分钟消除
     rs.set(id + '_salaryType', data['salaryType'], ex=600)
     rs.set(id + '_salary', data['salary'], ex=600)
-    return "0"
+    return id
 
-# @app.route('/join_room', methods=["POST"])
-# def join_room(room_num, salary):
-#     total = rs.get(room_num)
-#     for i in range(1, total):
-#         if rs.get(str(idx) + '_' + str(i)):
-#             continue
-#         rs.setnx(str(idx) + '_' + str(i), salary, ex=300)
-#     if rs.get(room_num + '_' + str(total - 1)):
-#         return True
-#     return False
+@app.route('/join_room', methods=["POST"])
+def join_room():
+    data = json.loads(request.get_data(as_text=True))
+    id = data['roomNumber']
+    room_number = rs.get(data['roomNumber'] + '_roomNumber')
+    if room_number:
+        salary = data['salary'] + int(salary)
+        rs.set(id + '_salary', str(salary), ex=600)
+        now_people = int(rs.get(id + 'nowPeople'))
+        if now_people + 1 == int(rs.get(id + '_roomPeople')):
+            return int(rs.get(id + '_salary')) / (now_people + 1)
+    return "房间号不存在或已失效"
 
 @app.route('/error')
 def error():
